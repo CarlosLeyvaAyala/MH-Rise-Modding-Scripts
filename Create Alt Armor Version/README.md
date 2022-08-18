a65d967d739b904bcae9b9c77c540f722c389376
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-08-18 18:16:31 -0500

    Read settings from config file

--- Create Alt Armor Version/README.md
+++ Create Alt Armor Version/README.md
@@ -1,4 +1,27 @@
-# Create Alt Version
+# Create Alt Armor Version
+
 F# script that creates alternative versions of armors by copying files to another folder, so they can be worked on them.
 
 If configured correctly it will create all relative paths for a file name.
+
+# Usage
+
+Drag and drop the files you want to create alternative versions of to the `drag_files_here.bat` file.
+
+You will be asked to provide a suffix for the new directory, then all dragged files will be copied to the newly created directory with all their needed folder structure.
+
+# Configuration
+
+By default, this script expects your mods to be in a Mod Organizer `mods` dir.
+
+For example, this is the kind of file structire it expects to get:
+
+    x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor\natives\STM\player\mod\f\pl352\f_body352.mesh.2109148288
+    x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor\natives\STM\player\mod\f\pl352\f_leg352.mesh.2109148288
+
+If you dragged those files to `drag_files_here.bat` and added the `_Skimpy` suffix, these would be the newly copied files:
+
+    x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor_Skimpy\natives\STM\player\mod\f\pl352\f_body352.mesh.2109148288
+    x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor_Skimpy\natives\STM\player\mod\f\pl352\f_leg352.mesh.2109148288
+
+If you want another folder structure to be recognized, you need to open the `script.fsx` file
--- Create Alt Armor Version/script.fsx
+++ Create Alt Armor Version/script.fsx
@@ -1,41 +1,13 @@
-open System.Text.RegularExpressions
-open System.IO
 open System
 open System.Diagnostics
+open System.IO
+open System.Text.RegularExpressions
+open System.Text.Json
 
-/// <summary>
-/// Base dir where all mods are.
-/// </summary>
-/// <remarks>
-/// <para>This is used so directory structures are correctly managed by
-/// this script.</para>
-///
-/// <para>By default, it expects mods to be worked on to be inside a folder named <c>"mods"</c>,
-/// like this:</para>
-///
-/// <code>
-/// x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor\natives\STM\player\mod\f\pl352\f_body352.mesh.2109148288
-/// x:\MH Rise\Mod.Organizer\mods\EBB Malzeno Armor\natives\STM\player\mod\f\pl352\f_leg352.mesh.2109148288
-/// </code>
-///
-/// <para>With this value being <c>"mods"</c>, <see cref="getFilePaths">getFilePaths</see> will
-/// recognize this structure:</para>
-/// <code>
-/// BasePath = x:\MH Rise\Mod.Organizer\mods\
-/// ModName = EBB Malzeno Armor
-/// RelPath = natives\STM\player\mod\f\pl352\
-/// FileName = f_leg352.mesh.2109148288
-/// </code>
-///
-/// <para>And then files will be copied where they are expected to be.</para>
-///
-/// <para>You can change this value to your liking so it correctly deals with whatevere setup
-/// you are dealing at the moment.</para>
-/// </remarks>
-let defaultBasePath = "mods"
+let serializeJson indented obj =
+    JsonSerializer.Serialize(obj, JsonSerializerOptions(WriteIndented = indented))
 
-/// Suffix added in case the user didn't provide one.
-let defaultModSuffix = " - Naked"
+type WarningList = string list
 
 type FileInfo =
     { OriginalFile: string
@@ -51,67 +23,159 @@ let blankInfo =
       RelPath = ""
       FileName = "" }
 
-/// Gets a simplified mod name.
-let cleanModName modName =
-    let m = Regex.Match(modName, @"(.*?)-.*")
+let strReplace oldStr newStr (str: string) =
+    str.Replace(oldValue = oldStr, newValue = newStr)
 
-    if m.Success then
-        m.Groups[ 1 ].Value.Trim()
+module Cfg =
+    let private filePath = Path.Combine(__SOURCE_DIRECTORY__, "config.json")
+
+    type Config =
+        { basePath: string
+          defaultModSuffix: string }
+
+    type ConfigData = { Data: Config; Warnings: WarningList }
+
+    let private defaultCfg =
+        { basePath = "Mod.Organizer-2.4.4/mods"
+          defaultModSuffix = " - Naked" }
+
+    type ConfigFileContent =
+        | FileContent of string
+        | FileNotFound of string
+        | UnknownFileError of string
+
+    let private readCfgFile () =
+        try
+            let f = File.ReadAllText(filePath)
+            FileContent f
+        with
+        | :? FileNotFoundException as e -> FileNotFound e.Message
+        | _ as e -> UnknownFileError e.Message
+
+    let private showUnexpectedError lst error =
+        lst
+        @ [ error
+            "Getting default configuration values due to unexpected error."
+            "" ]
+
+    let private ensureFileContent content =
+        let d = serializeJson true defaultCfg
+
+        match content with
+        | FileNotFound error ->
+            let newCfgMsg = "A new configuration file with default values was created."
+            File.WriteAllText(path = filePath, contents = d)
+            ([ error; newCfgMsg; "" ], d)
+        | UnknownFileError e -> (showUnexpectedError [] e, d)
+        | FileContent c -> ([], c)
+
+    let private strToConfig (c: WarningList * string) =
+        try
+            let s = snd c
+            let o = JsonSerializer.Deserialize<Config> s
+            { Data = o; Warnings = fst c }
+        with
+        | _ as e ->
+            { Data = defaultCfg
+              Warnings = showUnexpectedError (fst c) e.Message }
+
+    let value =
+        ()
+        |> readCfgFile
+        |> ensureFileContent
+        |> strToConfig
+
+module ProcessFiles =
+    let cfg = Cfg.value.Data
+
+    module private RegexOps =
+        let normalizePath (path: string) =
+            let join s1 s2 = s1 + s2
+
+            Path.TrimEndingDirectorySeparator(path) |> join
+            <| "/"
+            |> strReplace "/" @"\\"
+
+        let n = normalizePath cfg.basePath
+        let rx = $"(.*{n})" + @"(.*?)\\(.*)\\(.*)"
+
+        /// Gets a simplified mod name.
+        let cleanModName modName =
+            let m = Regex.Match(modName, @"(.*?)-.*")
+
+            if m.Success then
+                m.Groups[ 1 ].Value.Trim()
+            else
+                modName
+
+    let getFilePaths filePath =
+        let m = Regex.Match(filePath, RegexOps.rx)
+
+        if m.Success then
+            { OriginalFile = filePath
+              BasePath = m.Groups[1].Value
+              ModName = RegexOps.cleanModName m.Groups[2].Value
+              RelPath = m.Groups[3].Value
+              FileName = m.Groups[4].Value }
+        else
+            blankInfo
+
+    /// Suffix added in case the user didn't provide one.
+    let defaultModSuffix = " - Naked"
+
+    let getNewFilePaths fi =
+        let modPath = Path.Combine(fi.ModName + cfg.defaultModSuffix, fi.RelPath)
+        let newDir = Path.Combine(fi.BasePath, modPath)
+        let newFile = Path.Combine(newDir, fi.FileName)
+        (newFile, newDir)
+
+
+    let processFilePaths operation fi =
+        let (newFile, newDir) = getNewFilePaths fi
+        operation newDir newFile fi.OriginalFile
+        newDir
+
+    let copyNewFile newDir newFile originalFile =
+        Directory.CreateDirectory(newDir) |> ignore
+        File.Delete(newFile)
+        File.Copy(sourceFileName = originalFile, destFileName = newFile)
+
+    let openDir (dirPath: string) =
+        Process.Start(fileName = "explorer.exe", arguments = dirPath)
+        |> ignore
+
+        dirPath
+
+module Mock =
+    let openDir dirPath =
+        printfn "Mock open dir\t%s" dirPath
+        dirPath
+
+    let copyNewFile _ (newFile: string) _ =
+        let p = Path.GetFileName(newFile)
+        printfn "Mock copy\t%s" p
+
+open ProcessFiles
+
+let run copyNewFile openDir (args: string array) =
+    if args.Length > 1 then
+        args[1..]
+        |> Array.toList
+        |> List.map getFilePaths
+        |> List.map (processFilePaths copyNewFile)
+        |> List.distinct
+        |> List.map openDir
+        |> List.iter (printfn "Files were copied to: %s")
     else
-        modName
+        printfn "You need to drag and drop at least one file"
+        printf "Press ENTER to continue..."
+        Console.Read() |> ignore
 
-let getFilePaths filePath =
-    let rx =
-        @"(.*\\"
-        + defaultBasePath
-        + @"\\)(.*?)\\(.*)\\(.*)"
+let validPaths =
+    [| "Dummy path"
+       @"F:\MH Rise\Mod.Organizer-2.4.4\mods\EBB Rajang Armor\natives\STM\player\mod\f\pl235\f_body352.mesh.2109148288"
+       @"F:\MH Rise\Mod.Organizer-2.4.4\mods\EBB Rajang Armor\natives\STM\player\mod\f\pl235\f_leg235.mesh.2109148288" |]
 
-    let m = Regex.Match(filePath, rx)
-
-    if m.Success then
-        { OriginalFile = filePath
-          BasePath = m.Groups[1].Value
-          ModName = cleanModName m.Groups[2].Value
-          RelPath = m.Groups[3].Value
-          FileName = m.Groups[4].Value }
-    else
-        blankInfo
-
-let getNewFilePaths fi =
-    let modPath = Path.Combine(fi.ModName + defaultModSuffix, fi.RelPath)
-    let newDir = Path.Combine(fi.BasePath, modPath)
-    let newFile = Path.Combine(newDir, fi.FileName)
-    (newFile, newDir)
-
-let processFilePaths operation fi =
-    let (newFile, newDir) = getNewFilePaths fi
-    operation newDir newFile fi.OriginalFile
-    newDir
-
-let copyNewFile newDir newFile originalFile =
-    Directory.CreateDirectory(newDir) |> ignore
-    File.Delete(newFile)
-    File.Copy(sourceFileName = originalFile, destFileName = newFile)
-
-let mockCopyNewFile _ (newFile: string) _ =
-    let p = Path.GetFileName(newFile)
-    printfn "Mock copy\t%s" p
-
-let openDir (dirPath: string) =
-    Process.Start(fileName = "explorer.exe", arguments = dirPath)
-    |> ignore
-
-    dirPath
-
-if fsi.CommandLineArgs.Length > 1 then
-    fsi.CommandLineArgs[1..]
-    |> Array.toList
-    |> List.map getFilePaths
-    |> List.map (processFilePaths mockCopyNewFile)
-    |> List.distinct
-    |> List.map openDir
-    |> List.iter (printfn "Files were copied to: %s")
-else
-    printfn "You need to drag and drop at least one file"
-    printf "Press ENTER to continue..."
-    Console.Read() |> ignore
+// run fsi.CommandLineArgs
+let runMock = run Mock.copyNewFile Mock.openDir
+runMock validPaths
