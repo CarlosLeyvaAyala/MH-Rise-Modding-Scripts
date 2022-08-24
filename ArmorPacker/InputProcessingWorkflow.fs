4c098c0dbcf2e3d6df80ca836387ac6813818209
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-08-24 14:13:01 -0500

    Make the app generate files from fileName

---
+++ ArmorPacker/InputProcessingWorkflow.fs
@@ -0,0 +1,38 @@
+﻿module InputProcessingWorkflow
+
+open Domain
+open System
+open System.IO
+
+let (|IsDir|_|) path =
+  match File.GetAttributes(path: string) with
+  | FileAttributes.Directory -> Some path
+  | _ -> None
+
+type EntryParams =
+  { InputDir: string
+    OutFile: string option }
+
+let private askForOutputFile () =
+  printfn "What will be the name of the release file?"
+  Console.ReadLine()
+
+let getInput args =
+  let t =
+    match Array.toList args with
+    | [] -> failwith "You must drag and drop a folder (or file) to this app."
+    | [ IsDir dir ] ->
+      { EntryParams.InputDir = dir
+        OutFile = None }
+    | [ file ] ->
+      { InputDir = Path.GetDirectoryName(file)
+        OutFile = Some(Path.GetFileNameWithoutExtension(file)) }
+    | dir :: file :: _ -> { InputDir = dir; OutFile = Some file }
+
+  match t.OutFile with
+  | Some o ->
+    { FullParams.InputDir = t.InputDir
+      OutFile = o }
+  | None ->
+    { InputDir = t.InputDir
+      OutFile = askForOutputFile () }
--- ArmorPacker/Program.fs
+++ ArmorPacker/Program.fs
@@ -1,38 +1,14 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
-open Domain
-
-type EntryParams =
-  { InputDir: string
-    OutFile: string option }
-
-let askForOutputFile () =
-  printfn "What will be the name of the release file?"
-  Console.ReadLine()
-
-let getInput args =
-  let t =
-    match Array.toList args with
-    | [] -> failwith "You must drag and drop a folder to this app."
-    | [ dir ] ->
-      { EntryParams.InputDir = dir
-        OutFile = None }
-    | dir :: file :: _ -> { InputDir = dir; OutFile = Some file }
-
-  match t.OutFile with
-  | Some o ->
-    { FullParams.InputDir = t.InputDir
-      OutFile = o }
-  | None ->
-    { InputDir = t.InputDir
-      OutFile = askForOutputFile () }
 
 [<EntryPoint>]
 let main args =
   let r =
     try
-      let input = getInput args
-      CompressWorkflow.execute input
+      args
+      |> InputProcessingWorkflow.getInput
+      |> CompressWorkflow.execute
+
       0
     with
     | _ as e ->
