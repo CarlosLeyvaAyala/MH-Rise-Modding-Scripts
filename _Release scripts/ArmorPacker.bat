fd878638cf87feeb9799976b95375fe8419806fa
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-09-13 04:00:25 -0500

    Update ArmorPacker.bat
    Max compression

--- _Release scripts/ArmorPacker.bat
+++ _Release scripts/ArmorPacker.bat
@@ -5,5 +5,5 @@ set zipExe="C:\Program Files\7-Zip\7z.exe"
 set releaseFile=".\%fileName% %version%.zip"
 del %releaseFile%
 
-%zipExe% a -t7z %releaseFile% "..\ArmorPacker\bin\Release\net6.0\*" -spf2
-%zipExe% a -t7z %releaseFile% "..\ArmorPacker\pack.bat" -spf2
+%zipExe% a -t7z %releaseFile% "..\ArmorPacker\bin\Release\net6.0\*" -spf2 -mx=9
+%zipExe% a -t7z %releaseFile% "..\ArmorPacker\pack.bat" -spf2 -mx=9
